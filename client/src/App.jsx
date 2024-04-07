28d2ab329907bea8f301ef3715d6b8e40e256f7e
Samarth <kayaniv321@gmail.com>
2024-04-08 00:53:18 +0530

    created footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,12 @@ import SignUp from './pages/SignUp.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Projects from './pages/Projects.jsx';
 import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />          
@@ -20,6 +21,7 @@ export default function App() {
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/projects' element={<Projects />} />   
         </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
