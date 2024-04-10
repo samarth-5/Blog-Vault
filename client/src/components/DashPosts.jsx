680afa946d43d89556eed1015c907bc872534aff
Samarth <kayaniv321@gmail.com>
2024-04-10 18:20:04 +0530

    added delete post route

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
-import { set } from 'mongoose';
 
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
@@ -22,18 +21,20 @@ export default function DashPosts() {
             setShowMore(false);
           }
         }
-      } catch (error) {
+      } 
+      catch (error) {
         console.log(error.message);
       }
     };
-    if (currentUser.isAdmin) {
+    if (currentUser.isAdmin) 
+    {
       fetchPosts();
     }
   }, [currentUser._id]);
 
   const handleShowMore = async () => {
     const startIndex = userPosts.length;
-    try {
+    try{
       const res = await fetch(
         `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
       );
@@ -44,7 +45,8 @@ export default function DashPosts() {
           setShowMore(false);
         }
       }
-    } catch (error) {
+    } 
+    catch (error) {
       console.log(error.message);
     }
   };
@@ -52,21 +54,22 @@ export default function DashPosts() {
   const handleDeletePost = async () => {
     setShowModal(false);
     try {
-      const res = await fetch(
-        `/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
+      const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,
         {
           method: 'DELETE',
         }
       );
       const data = await res.json();
-      if (!res.ok) {
+      if (!res.ok) 
+      {
         console.log(data.message);
-      } else {
-        setUserPosts((prev) =>
-          prev.filter((post) => post._id !== postIdToDelete)
-        );
+      } 
+      else 
+      {
+        setUserPosts((prev) => prev.filter((post) => post._id !== postIdToDelete));
       }
-    } catch (error) {
+    } 
+    catch (error) {
       console.log(error.message);
     }
   };
@@ -94,38 +97,26 @@ export default function DashPosts() {
                   </Table.Cell>
                   <Table.Cell>
                     <Link to={`/post/${post.slug}`}>
-                      <img
-                        src={post.image}
-                        alt={post.title}
-                        className='w-20 h-10 object-cover bg-gray-500'
-                      />
+                      <img src={post.image} alt={post.title} className='w-20 h-10 object-cover bg-gray-500' />
                     </Link>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link
-                      className='font-medium text-gray-900 dark:text-white'
-                      to={`/post/${post.slug}`}
-                    >
+                    <Link className='font-medium text-gray-900 dark:text-white' to={`/post/${post.slug}`}>
                       {post.title}
                     </Link>
                   </Table.Cell>
                   <Table.Cell>{post.category}</Table.Cell>
                   <Table.Cell>
-                    <span
-                      onClick={() => {
+                    <span onClick={() => {
                         setShowModal(true);
                         setPostIdToDelete(post._id);
                       }}
-                      className='font-medium text-red-500 hover:underline cursor-pointer'
-                    >
+                      className='font-medium text-red-500 hover:underline cursor-pointer'>
                       Delete
                     </span>
                   </Table.Cell>
                   <Table.Cell>
-                    <Link
-                      className='text-teal-500 hover:underline'
-                      to={`/update-post/${post._id}`}
-                    >
+                    <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
                       <span>Edit</span>
                     </Link>
                   </Table.Cell>
@@ -134,10 +125,7 @@ export default function DashPosts() {
             ))}
           </Table>
           {showMore && (
-            <button
-              onClick={handleShowMore}
-              className='w-full text-teal-500 self-center text-sm py-7'
-            >
+            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
               Show more
             </button>
           )}
@@ -145,12 +133,7 @@ export default function DashPosts() {
       ) : (
         <p>You have no posts yet!</p>
       )}
-      <Modal
-        show={showModal}
-        onClose={() => setShowModal(false)}
-        popup
-        size='md'
-      >
+      <Modal show={showModal} onClose={() => setShowModal(false)} popup size='md'>
         <Modal.Header />
         <Modal.Body>
           <div className='text-center'>
--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -58,3 +58,15 @@ export const getPosts=async(req,res,next)=>{
         next(err);
     }
 }
+
+export const deletePost=async(req,res,next)=>{
+    if(!req.user.isAdmin || req.user.id!==req.params.userId)
+    res.status(200).json('The post has been deleted!');
+    try{
+        await Post.findByIdAndDelete(req.params.postId);
+        res.status(200).json('The post has been deleted!');
+    }
+    catch(err){
+        next(err);
+    }
+}
