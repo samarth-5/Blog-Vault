62232b22688b6763eeff830a283fdfa677d2aa0c
Samarth <kayaniv321@gmail.com>
2024-04-14 00:19:07 +0530

    created about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Samarth' Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to Samarth's Blog! Lorem ipsum dolor sit amet consectetur adipisicing 
+              elit. Corporis in qui quisquam nemo quod numquam sequi cupiditate necessitatibus, 
+              nesciunt ab illum maxime culpa, velit voluptatem accusamus. Veritatis quae fugit vel.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui mollitia tempore fugit 
+              magni neque blanditiis optio ullam. Architecto pariatur libero consectetur asperiores 
+              illum. Totam officiis culpa alias animi excepturi sequi.lorem Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui mollitia tempore fugit 
+              magni neque blanditiis optio ullam. Architecto pariatur libero consectetur asperiores 
+              illum. Totam officiis culpa alias animi excepturi sequi.lorem
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui mollitia tempore fugit 
+              magni neque blanditiis optio ullam. Architecto pariatur libero consectetur asperiores 
+              illum. Totam officiis culpa alias animi excepturi sequi.loremLorem ipsum dolor sit amet consectetur, adipisicing elit. Qui mollitia tempore fugit 
+              magni neque blanditiis optio ullam. Architecto pariatur libero consectetur asperiores 
+              illum. Totam officiis culpa alias animi excepturi sequi.lorem
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
