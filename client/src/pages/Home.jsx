4feabb2a4fbf37c38e30854112e7b66f447f2dad
Samarth <kayaniv321@gmail.com>
2024-04-13 22:22:35 +0530

    complete home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom';
+import CallToAction from '../components/CallToAction.jsx';
+import PostCard from '../components/PostCard.jsx';
 
 export default function Home() {
+  const [posts,setPosts]=useState([]);
+
+  useEffect(()=>{
+    const fetchPosts=async()=>{
+      const res=await fetch('/api/post/getposts');
+      const data=await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  },[]);
+
   return (
-    <div>Home</div>
+    <div>
+      <div className='flex flex-col gap-6 lg:p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my blog...</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor repellendus 
+          aspernatur eveniet incidunt sequi provident dolore vitae nisi porro earum quasi 
+          alias sapiente, nihil et non saepe cumque aliquid? Quasi.</p>
+        <Link to='/search' className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>
+          View all posts
+        </Link>
+      </div>
+      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+        <CallToAction />
+      </div>
+      <div className='max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7'>
+        {
+          posts && posts.length>0 && (
+            <div className='flex flex-col gap-6'>
+              <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+              <div className='flex flex-wrap gap-4'>
+                {posts.map((post)=>(
+                  <PostCard key={post._id} post={post} />
+                ))}
+              </div>
+              <Link to='/search' className='text-lg text-teal-500 hover:underline text-center'>
+                View all Posts
+              </Link>
+            </div>
+          )
+        }
+      </div>
+    </div>
   )
 }
